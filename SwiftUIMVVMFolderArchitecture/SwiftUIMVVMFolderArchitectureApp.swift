3ac4a84abe5cedcd6eec5e210b54a27ea506499e
Shahin <msrahman.mail@gmail.com>
2025-05-06 16:37:33 +0600

    Initial Commit

---
+++ SwiftUIMVVMFolderArchitecture/SwiftUIMVVMFolderArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMVVMFolderArchitectureApp.swift
+//  SwiftUIMVVMFolderArchitecture
+//
+//  Created by Shahinur Rahman on 6/5/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMVVMFolderArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIMVVMFolderArchitectureTests/SwiftUIMVVMFolderArchitectureTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMVVMFolderArchitectureTests.swift
+//  SwiftUIMVVMFolderArchitectureTests
+//
+//  Created by Shahinur Rahman on 6/5/25.
+//
+
+import Testing
+@testable import SwiftUIMVVMFolderArchitecture
+
+struct SwiftUIMVVMFolderArchitectureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
